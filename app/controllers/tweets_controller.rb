ba9902874d0cc81836c1dbca9a13ef75b44b2f81
yamawo <y7a3m1asaki@gmail.com>
2019-08-24 14:16:05 +0900

    added post

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,20 @@
 class TweetsController < ApplicationController
 
     def index
+        @tweets = Tweet.all
+    end
+
+    def new
+
+    end
+
+    def create
+        Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+        params.permit(:name, :image, :text)
     end
 
 end
